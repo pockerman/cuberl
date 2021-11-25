48b9b12e87fea0ef1dd58a4e5e7a5cfafddcad0e
Alex <a.giavaras@gmail.com>
2021-11-25 21:34:10 +0000

    Add PG source

---
+++ src/cubeai/rl/algorithms/pg/vanilla_reinforce.cpp
@@ -0,0 +1,26 @@
+#include "cubeai/rl/algorithms/pg/vanilla_reinforce.h"
+
+namespace cubeai {
+namespace rl {
+namespace algos {
+namespace pg {
+
+std::ostream&
+ReinforceOpts::print(std::ostream& out)const{
+
+    out<<"Max num episodes=    "<<max_num_of_episodes<<std::endl;
+    out<<"Max its per episode= "<<max_itrs_per_episode<<std::endl;
+    out<<"Print frequency=     "<<print_frequency<<std::endl;
+    out<<"Score queue maz size="<<scores_queue_max_size<<std::endl;
+    out<<"Gamma=               "<<gamma<<std::endl;
+    out<<"Exit tolerance=      "<<tolerance<<std::endl;
+    out<<"Exit score level=    "<<exit_score_level<<std::endl;
+    return out;
+}
+
+}
+}
+}
+}
+
+
