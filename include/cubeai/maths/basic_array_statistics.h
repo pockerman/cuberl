f62e6c9354c6b6a8585a856ea176c621411650e1
pockerman <a.giavaras@gmail.com>
2024-02-24 11:41:12 +0000

    Update CMakeLists added lambda_utils

--- include/cubeai/maths/basic_array_statistics.h
+++ include/cubeai/maths/basic_array_statistics.h
@@ -10,6 +10,8 @@
 #include <algorithm>
 #include <execution>
 #include <random>
+#include <iterator>
+#include <iostream>
 
 namespace cubeai{
 namespace maths{
@@ -26,7 +28,6 @@ mean(const VectorType& vector, bool parallel=true){
 
     if(parallel){
       sum   = std::reduce(std::execution::par, vector.begin(), vector.end(), sum);
-
     }
     else{
         sum = std::accumulate(vector.begin(), vector.end(), sum);
@@ -59,6 +60,7 @@ choice(const Vec2& probs, uint_t seed=42){
     return distribution(generator);
 }
 
+
 ///
 /// \brief choice. Implements similar functionality
 /// to numpy.choice function
@@ -72,6 +74,34 @@ choice(const Vec1& choices, const Vec2& probs, uint_t seed=42){
     std::mt19937 generator(seed);
     std::discrete_distribution<int> distribution(probs.begin(), probs.end());
     return choices[distribution(generator)];
+}
+
+
+template<utils::concepts::float_vector Vec>
+typename Vec::value_type
+choose_value(const Vec& vals, uint_t seed=42){
+
+    std::mt19937 generator(seed);
+    auto size = std::distance(vals.begin(), vals.end());
+    std::discrete_distribution<int> distribution(0, static_cast<int>(size));
+    auto rand_idx = distribution(generator);
+    return vals[rand_idx];
+}
+
+///
+/// \brief Given a vector of intergal or floating point values
+/// and a set of values to choose from, randomize the entries ofv
+///
+template<utils::concepts::float_or_integral_vector Vec>
+void
+randomize_vec(Vec& v, const Vec& walk_set, uint_t seed=42){
+
+
+    std::for_each(v.begin(), v.end(),
+                  [&](auto& val){
+                      val +=  choose_value(walk_set, seed);
+                  });
+
 }
 
 
