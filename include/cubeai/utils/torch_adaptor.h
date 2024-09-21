1d46119aa5ef0cc26fedb599cec0b3b25da0f787
pockerman <a.giavaras@gmail.com>
2024-09-21 09:20:58 +0100

    Fix multiple definitions linking error

--- include/cubeai/utils/torch_adaptor.h
+++ include/cubeai/utils/torch_adaptor.h
@@ -21,7 +21,6 @@ struct TorchAdaptor{
 
 
     typedef torch_tensor_t value_type;
-    typedef torch_tensor_t state_type;
 
     torch_tensor_t operator()(real_t value)const;
     torch_tensor_t operator()(const std::vector<real_t>& data)const;
@@ -32,55 +31,6 @@ struct TorchAdaptor{
     template<typename T>
     static std::vector<T> to_vector(torch_tensor_t tensor);
 };
-
-
-template<>
-std::vector<int>
-TorchAdaptor::to_vector(torch_tensor_t tensor){
-
-    auto cont_t = tensor.contiguous();
-    return std::vector<int>(cont_t.data_ptr<int>(),
-                              cont_t.data_ptr<int>() + cont_t.numel());
-}
-
-template<>
-std::vector<uint_t>
-TorchAdaptor::to_vector(torch_tensor_t tensor){
-
-    auto cont_t = tensor.contiguous();
-    return std::vector<uint_t>(cont_t.data_ptr<long int>(),
-                              cont_t.data_ptr<long int>() + cont_t.numel());
-}
-
-#ifdef CUBEAI_REAL_TYPE_FLOAT
-template<>
-std::vector<float>
-TorchAdaptor::to_vector(torch_tensor_t tensor){
-
-    auto cont_t = tensor.contiguous();
-    return std::vector<float>(cont_t.data_ptr<float>(),
-                              cont_t.data_ptr<float>() + cont_t.numel());
-}
-
-#else
-template<>
-std::vector<float>
-TorchAdaptor::to_vector(torch_tensor_t tensor){
-
-    auto cont_t = tensor.contiguous();
-    return std::vector<float>(cont_t.data_ptr<float>(),
-                              cont_t.data_ptr<float>() + cont_t.numel());
-}
-
-template<>
-std::vector<real_t>
-TorchAdaptor::to_vector(torch_tensor_t tensor){
-
-    auto cont_t = tensor.contiguous();
-    return std::vector<real_t>(cont_t.data_ptr<real_t>(),
-                              cont_t.data_ptr<real_t>() + cont_t.numel());
-}
-#endif
 
 }
 }
--- src/cubeai/utils/torch_adaptor.cpp
+++ src/cubeai/utils/torch_adaptor.cpp
@@ -9,7 +9,7 @@ namespace torch_utils{
 
 torch_tensor_t
 TorchAdaptor::operator()(real_t value)const{
-    return this->operator()(std::vector<real_t>(1, value));
+    return operator()(std::vector<real_t>(1, value));
 }
 
 torch_tensor_t
@@ -19,7 +19,6 @@ TorchAdaptor::operator()(const std::vector<real_t>& data)const{
 
 torch_tensor_t
 TorchAdaptor::operator()(const std::vector<int>& data)const{
-
     return torch::tensor(data);
 };
 
@@ -27,6 +26,43 @@ TorchAdaptor::value_type
 TorchAdaptor::stack(const std::vector<value_type>& values)const{
 
     return torch::stack(values, 0);
+}
+
+
+template<>
+std::vector<int>
+TorchAdaptor::to_vector(torch_tensor_t tensor){
+
+    auto cont_t = tensor.contiguous();
+    return std::vector<int>(cont_t.data_ptr<int>(),
+                              cont_t.data_ptr<int>() + cont_t.numel());
+}
+
+template<>
+std::vector<uint_t>
+TorchAdaptor::to_vector(torch_tensor_t tensor){
+
+    auto cont_t = tensor.contiguous();
+    return std::vector<uint_t>(cont_t.data_ptr<long int>(),
+                              cont_t.data_ptr<long int>() + cont_t.numel());
+}
+
+template<>
+std::vector<float>
+TorchAdaptor::to_vector(torch_tensor_t tensor){
+
+    auto cont_t = tensor.contiguous();
+    return std::vector<float>(cont_t.data_ptr<float>(),
+                              cont_t.data_ptr<float>() + cont_t.numel());
+}
+
+template<>
+std::vector<double>
+TorchAdaptor::to_vector(torch_tensor_t tensor){
+
+    auto cont_t = tensor.contiguous();
+    return std::vector<real_t>(cont_t.data_ptr<double>(),
+                              cont_t.data_ptr<double>() + cont_t.numel());
 }
 
 }
